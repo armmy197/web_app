63ece19368c9c208a5f24f2e9938052f1f2a3554
armmy197 <patinyathi29@gmail.com>
2026-01-03 21:44:44 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,7 +11,3 @@ mkdir -p save_data/certificates_files
 
 # Install Google Sheets dependencies
 pip install --upgrade gspread google-auth google-auth-oauthlib google-auth-httplib2 google-api-python-client oauth2client
-
-# Create necessary files
-touch save_data/lessons/.gitkeep
-touch save_data/quiz_results/.gitkeep
